5f6ee490131eaff3b1e96d518021204ab9d2cb8f
Yiping Han <yhan@yahoo-inc.com>
2015-02-10 22:33:55 -0800

    functional version of multiplication matrix as in the book

--- src/main/scala/test2/Chapter7.scala
+++ src/main/scala/test2/Chapter7.scala
@@ -3,6 +3,51 @@ package test2
 /**
  * Created by yhan on 2/9/15.
  */
-class Chapter7 {
+class Chapter7{
+
+
+}
+
+object Chapter7 extends App{
+val list = for {
+    i <- 1 to 5
+    if (i != 3)
+    a = println("*"*i)
+    j <- 1 to 5
+    if (j != i)
+    b = println("#"*j)
+  } yield i*j
+  try {
+    list.foreach(println)
+    throw new IllegalAccessException("Sample Exception")
+  }catch {
+    case ex:IllegalAccessException => println("No problem, caught.")
+  }finally {
+    println("Well, never forget this.")
+  }
+
+  3 match {
+    case 3 => println(3)
+    case 5 => println("no")
+  }
+
+  def makeRowSeq(row: Int) = {
+    for (col <- 1 to 10) yield {
+      val prod = (row * col).toString
+      val padding = " " * (4 - prod.length)
+      padding + prod
+    }
+  }
+
+  def makeRow(row:Int) = makeRowSeq(row).mkString
+
+  def multiTable() = {
+    val tableSeq =
+      for (row <- 1 to 10)
+        yield makeRow(row)
+    tableSeq.mkString("\n")
+  }
+
+  print(multiTable())
 
 }
